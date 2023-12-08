c6b2825e50f7d5d4bcc8c82cd4671f2c5616cde7
whit3vill <31686076+whit3vill@users.noreply.github.com>
2023-12-08 21:04:42 +0900

    Update elastic.py

--- elastic.py
+++ elastic.py
@@ -41,10 +41,14 @@ for col in df.columns:
     header.append(col.lstrip())
 df.columns = header
 
+
+'''
 if "Monday" in INPUT_FILE:
     df["Timestamp"] = pd.to_datetime(df["Timestamp"], format="%d/%m/%Y %H:%M:%S")
 else:
     df["Timestamp"] = pd.to_datetime(df["Timestamp"], format="%d/%m/%Y %H:%M")
+'''
+df["Timestamp"] = pd.to_datetime(df["Timestamp"], format="%Y-%m-%d %H:%M:%S")
 
 # Fill inf values with NaN
 df.replace([np.inf, -np.inf], np.nan, inplace=True)
@@ -96,6 +100,69 @@ for idx, row in tqdm(df.iterrows(), total=df.shape[0]):
         "Flow": {
             "flow_id": row["Flow ID"],
             "down_up_ratio": row["Down/Up Ratio"],
+            "fwd": {
+                "psh_flags": row["Fwd PSH Flags"],
+                "urg_flags": row["Fwd URG Flags"],
+                "header_bytes": row["Fwd Header Length"],
+                "header_length": row["Fwd Header Length.1"],
+                "packets/s": row["Fwd Packets/s"],
+                "init_win_bytes": row["Init_Win_bytes_forward"],
+                "act_data_pkt": row["act_data_pkt_fwd"],
+                "min_segment_size": row["min_seg_size_forward"],
+                "packet_length": {
+                    "max": row["Fwd Packet Length Max"],
+                    "min": row["Fwd Packet Length Min"],
+                    "mean": row["Fwd Packet Length Mean"],
+                    "std": row["Fwd Packet Length Std"]
+                },
+                "IAT": {
+                    "total": row["Fwd IAT Total"],
+                    "max": row["Fwd IAT Max"],
+                    "min": row["Fwd IAT Min"],
+                    "mean": row["Fwd IAT Mean"],
+                    "std": row["Fwd IAT Std"]
+                },
+                "avg": {
+                    "segment_size": row["Avg Fwd Segment Size"],
+                    "bytes/bulk": row["Fwd Avg Bytes/Bulk"],
+                    "packets/bulk": row["Fwd Avg Packets/Bulk"],
+                    "bulk_rate": row["Fwd Avg Bulk Rate"],
+                },
+                "subflow": {
+                    "packets": row["Subflow Fwd Packets"],
+                    "bytes": row["Subflow Fwd Bytes"],
+                }
+            },
+            "bwd": {
+                "psh_flags": row["Bwd PSH Flags"],
+                "urg_flags": row["Bwd URG Flags"],
+                "header_bytes": row["Bwd Header Length"],
+                "packets/s": row["Bwd Packets/s"],
+                "init_win_bytes": row["Init_Win_bytes_backward"],
+                "packet_length": {
+                    "max": row["Bwd Packet Length Max"],
+                    "min": row["Bwd Packet Length Min"],
+                    "mean": row["Bwd Packet Length Mean"],
+                    "std": row["Bwd Packet Length Std"]
+                },
+                "IAT": {
+                    "total": row["Bwd IAT Total"],
+                    "max": row["Bwd IAT Max"],
+                    "min": row["Bwd IAT Min"],
+                    "mean": row["Bwd IAT Mean"],
+                    "std": row["Bwd IAT Std"]
+                },
+                "avg": {
+                    "segment_size": row["Avg Bwd Segment Size"],
+                    "bytes/bulk": row["Bwd Avg Bytes/Bulk"],
+                    "packets/bulk": row["Bwd Avg Packets/Bulk"],
+                    "bulk_rate": row["Bwd Avg Bulk Rate"],
+                },
+                "subflow": {
+                    "packets": row["Subflow Bwd Packets"],
+                    "bytes": row["Subflow Bwd Bytes"],
+                }
+            },
             "flow": {
                 "bytes/s": row["Flow Bytes/s"],
                 "packets/s": row["Flow Packets/s"],
